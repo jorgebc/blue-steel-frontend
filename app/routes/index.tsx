40c6960eed461ec2c0ebdd85f1788d74e8911176
Jorge Santiago Buffa Casalis <jorge.buffa.casalis@gmail.com>
2022-04-26 17:57:29 +0200

    auth0 config

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,52 @@
-export default function Index() {
+import type {LoaderFunction} from '@remix-run/node'
+import type {Auth0Profile} from 'remix-auth-auth0'
+
+import {json} from '@remix-run/node'
+import {Form, useLoaderData, useCatch} from '@remix-run/react'
+
+import {auth} from '~/utils/auth.server'
+
+type LoaderData = {profile: Auth0Profile}
+
+export const loader: LoaderFunction = async ({request}) => {
+  const profile = await auth.isAuthenticated(request, {
+    failureRedirect: '/login',
+  })
+
+  return json<LoaderData>({profile})
+}
+
+export default function Screen() {
+  const {profile} = useLoaderData<LoaderData>()
   return (
-    <div style={{fontFamily: 'system-ui, sans-serif', lineHeight: '1.4'}}>
-      <h1>Welcome to blue steel frontend</h1>
-    </div>
+    <>
+      <Form method="post" action="/logout">
+        <button>Log Out</button>
+      </Form>
+
+      <hr />
+      <h1>Welcome to blue steel</h1>
+
+      <pre>
+        <code>{JSON.stringify(profile, null, 2)}</code>
+      </pre>
+    </>
   )
+}
+
+export function CatchBoundary() {
+  const caught = useCatch()
+
+  switch (caught.status) {
+    default:
+      return (
+        <div>
+          <h1>Caught</h1>
+          <p>Status: {caught.status}</p>
+          <pre>
+            <code>{JSON.stringify(caught.data, null, 2)}</code>
+          </pre>
+        </div>
+      )
+  }
 }
