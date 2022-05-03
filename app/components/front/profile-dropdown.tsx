839e63b9fed9c985947dab26325d3224b2327642
Jorge Santiago Buffa Casalis <jorge.buffa.casalis@gmail.com>
2022-05-03 20:25:42 +0200

    basic backend route

--- app/components/front/profile-dropdown.tsx
+++ app/components/front/profile-dropdown.tsx
@@ -1,9 +1,28 @@
 import type {Auth0Profile} from 'remix-auth-auth0'
 
 import {Fragment} from 'react'
+import {Link} from '@remix-run/react'
 import {Menu, Transition} from '@headlessui/react'
 
 import {classNames} from '~/utils'
+
+function ProfileMenuItem({to, label}: {label: String; to: string}) {
+  return (
+    <Menu.Item>
+      {({active}) => (
+        <Link
+          to="/back"
+          className={classNames(
+            active ? 'bg-gray-100' : '',
+            'block px-4 py-2 text-sm text-gray-700',
+          )}
+        >
+          {label}
+        </Link>
+      )}
+    </Menu.Item>
+  )
+}
 
 export function ProfileDropdown({profile}: {profile: Auth0Profile}) {
   return (
@@ -28,19 +47,8 @@ export function ProfileDropdown({profile}: {profile: Auth0Profile}) {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <Menu.Item>
-            {({active}) => (
-              <a
-                href="/logout"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'block px-4 py-2 text-sm text-gray-700',
-                )}
-              >
-                Salir
-              </a>
-            )}
-          </Menu.Item>
+          <ProfileMenuItem to="/back" label="Back" />
+          <ProfileMenuItem to="/logout" label="Salir" />
         </Menu.Items>
       </Transition>
     </Menu>
