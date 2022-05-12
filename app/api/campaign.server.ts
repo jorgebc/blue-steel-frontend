81de0ddd9c270ea9dc8194297cdd037a57c6a5d6
Jorge Santiago Buffa Casalis <jorge.buffa.casalis@gmail.com>
2022-05-12 17:41:26 +0200

    update types

--- app/api/campaign.server.ts
+++ app/api/campaign.server.ts
@@ -46,12 +46,12 @@ const getCampaignsQuery = gql`
         }
         auditingMetadata {
           createdBy {
-            id
             name
+            imageUrl
           }
           lastModifiedBy {
-            id
             name
+            imageUrl
           }
           creationDate
           lastModifiedDate
